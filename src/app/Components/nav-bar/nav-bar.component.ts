76eb1b8932827ee7dce3bd0af8bbbf1776bc2ef4
axl-bit <66146905+axl-bit@users.noreply.github.com>
2023-04-09 21:00:57 -0500

    change: adding navbar responsive and scss

--- src/app/Components/nav-bar/nav-bar.component.ts
+++ src/app/Components/nav-bar/nav-bar.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.css']
+  styleUrls: ['./nav-bar.component.scss']
 })
 export class NavBarComponent implements OnInit {
+
+  //varibale
+  isMenuOpen = false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  //for responsive
+  toggleMenu(){
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  navbarfixed:boolean = false;
+
 
 }
