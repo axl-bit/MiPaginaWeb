27815029ad9b6e15005ac37afe768afe55a2f288
axl-bit <66146905+axl-bit@users.noreply.github.com>
2023-04-11 00:35:42 -0500

    add: footer component, change: profile component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProfileComponent } from './Components/profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  //creamos la ruta para la pagina de perfil o inicio
+  {path: 'Inicio', component:ProfileComponent},
+  {path: '', redirectTo: 'Inicio', pathMatch:'full'}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+
 import { NavBarComponent } from './Components/nav-bar/nav-bar.component';
 import { ProfileComponent } from './Components/profile/profile.component';
+import { FooterComponent } from './Components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    ProfileComponent
+    ProfileComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
